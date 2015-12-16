c4578aabfc057e04322433604774df535a58f48b
Sean Kennedy <kennedymsean@gmail.com>
2015-12-16 00:43:58 -0500

    Updated syntax from deprecate state module to conntrack, added mysql and postgres, fixed proper routing for outgoing connections

--- iptables.sh
+++ iptables.sh
@@ -14,46 +14,73 @@ echo "Allow loopback"
 $ipt -A INPUT -i lo -j ACCEPT
 $ipt -A OUTPUT -o lo -j ACCEPT
 
+echo "Allow established/related inputs"
+$ipt -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+echo "Allow established outputs"
+$ipt -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+echo "Drop invalid packets"
+$ipt -A INPUT -m conntrack --ctstate INVALID -j DROP
+
 for var;
 do
     if [ $var == "http" ]; then
-        echo "Adding http(tcp:80) rules"
-        $ipt -A INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-        $ipt -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+        echo "Adding incoming http(tcp:80) rules"
+        $ipt -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+        echo "Adding outgoing http(tcp:80) rules"
+        $ipt -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
     fi
 
     if [ $var == "https" ]; then
-        echo "Adding https(tcp:443) rules"
-        $ipt -A INPUT -i eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-        $ipt -A OUTPUT -o eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
+        echo "Adding incoming https(tcp:443) rules"
+        $ipt -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
     fi
 
     if [ $var == "smtp" ]; then
-        echo "Adding smtp(tcp:25) rules"
-        $ipt -A INPUT -i eth0 -p tcp --dport 25 -m state --state NEW,ESTABLISHED -j ACCEPT
-        $ipt -A OUTPUT -o eth0 -p tcp --sport 25 -m state --state ESTABLISHED -j ACCEPT
+        echo "Adding smtp(tcp:25,587) rules"
+        $ipt -A INPUT -p tcp --dport 25 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p tcp --dport 25 -j ACCEPT
+        $ipt -A OUTPUT -p tcp --dport 587 -j ACCEPT
     fi
 
     if [ $var == "dns" ]; then
         echo "Adding dns(udp:53) rules"
-        $ipt -A INPUT -i eth0 -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-        $ipt -A OUTPUT -o eth0 -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p udp --dport 53 -j ACCEPT
     fi
 
     if [ $var == "ssh" ]; then
-        echo "Adding ssh(tcp:22) rules"
-        $ipt -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW -m recent --set --name SSHERS
-        $ipt -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW -m recent --update --seconds 300 --hitcount 4 --name SSHERS -j DROP
-        $ipt -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-        $ipt -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+        echo "Adding incoming ssh(tcp:22) rules"
+        $ipt -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSHERS
+        $ipt -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 300 --hitcount 4 --name SSHERS -j DROP
+        $ipt -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+        echo "Adding outgoing ssh(tcp:22) rules"
+        $ipt -A OUTPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+    fi
+
+	
+    if [ $var == "postgres" ]; then
+        echo "Adding postgres(tcp:5432) rules"
+        $ipt -A INPUT -p tcp --dport 5432 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p tcp --sport 5432 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+    fi
+
+    if [ $var == "mysql" ]; then
+        echo "Adding mysql(tcp:3306) rules"
+        $ipt -A INPUT -p tcp --dport 3306 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        $ipt -A OUTPUT -p tcp --sport 3306 -m conntrack --ctstate ESTABLISHED -j ACCEPT
     fi
 done
 
 echo "Add logging"
 $ipt -N CUSTOMLOG
 $ipt -A INPUT -j CUSTOMLOG
+$ipt -A OUTPUT -j CUSTOMLOG
 $ipt -A FORWARD -j CUSTOMLOG
-$ipt -A CUSTOMLOG -m limit --limit 2/min -j LOG --log-prefix "ipt packet drop: " --log-level 7
+$ipt -A CUSTOMLOG -m limit --limit 2/min -j LOG --log-prefix "ipt packet: " --log-level 7
 $ipt -A CUSTOMLOG -j DROP
 
 exit 0
